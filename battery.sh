b9d74b18d8d7eb7d22a2d42c5c6ab76f4102c4d0
SzymonKubica <szymo.kubica@gmail.com>
2022-12-10 20:17:40 +0000

    Fixed the bugs with string comparisons

--- battery.sh
+++ battery.sh
@@ -3,23 +3,23 @@ state=$(upower -i $(upower -e | grep 'BAT') | grep -E "state" | awk '{print $2}'
 
 charge_level="${percentage%%[%]*}"
 
-if [[ $((charge_level)) < 30 && "$state" != "charging" ]]
+if [[ $((charge_level)) -lt 30 && "$state" == "discharging" ]]
 then
   swaynag -m "Low battery, please charge now." --background 09090950 --border-bottom AF0000 --button-background 09090950 --button-border-size 2
 fi
 
 battery_icon=""
 
-if [[ $((charge_level)) > 90 ]]
+if [[ $((charge_level)) -gt 90 ]]
 then
   battery_icon=""
-elif [[ $((charge_level)) > 75 ]]
+elif [[ $((charge_level)) -gt 60 ]]
 then
   battery_icon=""
-elif [[ $((charge_level)) > 40 ]]
+elif [[ $((charge_level)) -gt 40 ]]
 then
   battery_icon=""
-elif [[ $((charge_level)) > 15 ]]
+elif [[ $((charge_level)) -gt 10 ]]
 then
   battery_icon=""
 else
